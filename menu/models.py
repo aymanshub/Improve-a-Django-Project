953a20c12f0a6910ad5f0d47d33bb80117680ce5
Ayman Said <ayman-said@live.com>
2019-10-31 10:59:39 +0200

    1. renamed those with date to datetime -> make migrations 2. added new date fields as date -> make migrations 3. create empty migration: 	python manage.py makemigrations --empty menu 4. add the data migration method

--- menu/models.py
+++ menu/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from _datetime import date
 
 
 class Menu(models.Model):
     season = models.CharField(max_length=20)
     items = models.ManyToManyField('Item', related_name='items')
-    created_date = models.DateField(
+    created_datetime = models.DateTimeField(
             default=timezone.now)
-    expiration_date = models.DateField(
+    expiration_datetime = models.DateTimeField(
             blank=True, null=True)
+    created_date = models.DateField(default=date.today)
+    expiration_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.season
@@ -18,10 +21,11 @@ class Item(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     chef = models.ForeignKey('auth.User')
-    created_date = models.DateField(
+    created_datetime = models.DateTimeField(
             default=timezone.now)
     standard = models.BooleanField(default=False)
     ingredients = models.ManyToManyField('Ingredient')
+    created_date = models.DateField(default=date.today)
 
     def __str__(self):
         return self.name
