0708f5633682b334b71614856dd84491f88bd525
Ayman Said <ayman-said@live.com>
2019-10-31 11:03:56 +0200

    removed datetime fields from models

--- menu/models.py
+++ menu/models.py
@@ -6,10 +6,6 @@ from _datetime import date
 class Menu(models.Model):
     season = models.CharField(max_length=20)
     items = models.ManyToManyField('Item', related_name='items')
-    created_datetime = models.DateTimeField(
-            default=timezone.now)
-    expiration_datetime = models.DateTimeField(
-            blank=True, null=True)
     created_date = models.DateField(default=date.today)
     expiration_date = models.DateField(blank=True, null=True)
 
@@ -21,8 +17,6 @@ class Item(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     chef = models.ForeignKey('auth.User')
-    created_datetime = models.DateTimeField(
-            default=timezone.now)
     standard = models.BooleanField(default=False)
     ingredients = models.ManyToManyField('Ingredient')
     created_date = models.DateField(default=date.today)
